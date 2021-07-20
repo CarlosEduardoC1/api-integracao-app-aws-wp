6c2967f8ae9229bb7aa9443d8a3bdaba3ef0b0db
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2021-07-20 08:19:39 -0300

    pqp

--- src/services/connect.js
+++ src/services/connect.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
 
-const connect = axios.create({ baseURL: `http://18.230.134.55:3080` });
+const connect = axios.create({ baseURL: `http://18.231.163.127:3080` });
 
 module.exports = connect;
